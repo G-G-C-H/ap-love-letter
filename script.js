6d1c2f8f0d0a6a4a20a58984c61071ed949bc96c
GG-CH <giorgia@wonderbly.com>
2020-11-20 14:33:23 +0000

    removed the litany function, added a generate button, added a background

---
+++ script.js
@@ -0,0 +1,78 @@
+var t = 0,
+  first = ['DARLING', 'DEAR', 'HONEY', 'JEWEL'],
+  second = ['DUCK', 'LOVE', 'MOPPET', 'SWEETHEART'],
+  adjectives = ['ADORABLE', 'AFFECTIONATE', 'AMOROUS', 'ANXIOUS', 'ARDENT', 'AVID', 'BREATHLESS', 'BURNING', 'COVETOUS', 'CRAVING', 'CURIOUS', 'DARLING', 'DEAR', 'DEVOTED', 'EAGER', 'EROTIC', 'FERVENT', 'FOND', 'IMPATIENT', 'KEEN', 'LITTLE',
+    'LOVEABLE', 'LOVESICK', 'LOVING', 'PASSIONATE', 'PRECIOUS', 'SWEET', 'SYMPATHETIC', 'TENDER', 'UNSATISFIED', 'WISTFUL'
+  ],
+  nouns = ['ADORATION', 'AFFECTION', 'AMBITION', 'APPETITE', 'ARDOUR', 'CHARM', 'DESIRE', 'DEVOTION', 'EAGERNESS', 'ENCHANTMENT', 'ENTHUSIASM', 'FANCY', 'FELLOW FEELING', 'FERVOUR', 'FONDNESS', 'HEART', 'HUNGER', 'INFATUATION', 'LIKING',
+    'LONGING', 'LOVE', 'LUST', 'PASSION', 'RAPTURE', 'SYMPATHY', 'TENDERNESS', 'THIRST', 'WISH', 'YEARNING'
+  ],
+  adverbs = ['AFFECTIONATELY', 'ANXIOUSLY', 'ARDENTLY', 'AVIDLY', 'BEAUTIFULLY', 'BREATHLESSLY', 'BURNINGLY', 'COVETOUSLY', 'CURIOUSLY', 'DEVOTEDLY', 'EAGERLY', 'FERVENTLY', 'FONDLY', 'IMPATIENTLY', 'KEENLY', 'LOVINGLY', 'PASSIONATELY',
+    'SEDUCTIVELY', 'TENDERLY', 'WINNINGLY', 'WISTFULLY'
+  ],
+  verbs = ['ADORES', 'ATTRACTS', 'CARES FOR', 'CHERISHES', 'CLINGS TO', 'DESIRES', 'HOLDS DEAR', 'HOPES FOR', 'HUNGERS FOR', 'IS WEDDED TO', 'LIKES', 'LONGS FOR', 'LOVES', 'LUSTS AFTER', 'PANTS FOR', 'PINES FOR', 'PRIZES', 'SIGHS FOR', 'TEMPTS',
+    'THIRSTS FOR', 'TREASURES', 'WANTS', 'WISHES', 'WOOS', 'YEARNS FOR'
+  ];
+
+// selects a random number from the array
+function rand_range(maximum) {
+  return Math.floor(Math.random() * (maximum + 1));
+}
+
+// prints the word of the array at the position picked by the rand range function
+function choose(array) {
+  return array[rand_range(array.length - 1)];
+}
+
+// maybe prints the word depending on what random number is picked
+function maybe(words) {
+  if (Math.random() > 0.5) {
+    return ' ' + choose(words);
+  }
+  return '';
+}
+
+// a longer sentence
+function longer() {
+  return ' MY' + maybe(adjectives) + ' ' + choose(nouns) + maybe(adverbs) + ' ' + choose(verbs) + ' YOUR' + maybe(adjectives) + ' ' + choose(nouns) + '.';
+}
+
+// a shorter sentence
+function shorter() {
+  return ' ' + choose(adjectives) + ' ' + choose(nouns) + '.';
+}
+
+// composes the letter
+function letter() {
+  var i, type, you_are = false,
+    text = '';
+  text = text + maybe(first) + ' ' + choose(second) + ',' + '<br /><br />';
+
+  for (i = 0; i < 5; i = i + 1) {
+    type = choose(['longer', 'shorter']);
+    // if longer gets chosen then print longer
+    if (type === 'longer') {
+      text = text + longer();
+      you_are = false;
+      // if shorter gets chosen either choose my + shorter or choose you are + shorter
+    } else {
+      if (you_are) {
+        text = text.slice(0, -1) + ': MY' + shorter();
+        you_are = false;
+      } else {
+        text = text + ' YOU ARE MY' + shorter();
+        you_are = true;
+      }
+    }
+  }
+  text = text +
+    '<br /><br />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;YOURS ' +
+    choose(adverbs) + '<br /><br />' +
+    '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;M.U.C.';
+  return text;
+}
+
+// a way of getting our letter function and reproducing it in html
+function myFunction() {
+  document.getElementById('div').innerHTML = letter();
+}
